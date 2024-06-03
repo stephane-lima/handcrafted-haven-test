51f4c96a5548b9acf3b2937572c529b58254da75
Stephane Lima <114018637+stephane-lima@users.noreply.github.com>
2024-06-03 17:58:55 -0100

    Update footer.tsx

--- src/app/ui/footer/footer.tsx
+++ src/app/ui/footer/footer.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Footer() {
     return (
         <footer>
-            <div className="bg-mainTitles text-background sm:px-3 md:px-36 py-6">
+            <div className="bg-mainTitles text-background px-6 py-6">
                 <h2 className="text-center text-3xl pb-3">About Handcrafted Haven</h2>
                 <p className="text-center text-lg">Welcome to Handcrafted Haven, where creativity meets craftsmanship! We are passionate artisans dedicated to curating and showcasing the finest handcrafted goods from around the world. At Handcrafted Haven, we believe that every piece tells a story. Each creation is infused with the love, dedication, and unique vision of the talented artisans behind it. From intricately designed jewelry to exquisitely crafted home decor, our platform is a celebration of artisanal mastery and creativity.</p>
                 
